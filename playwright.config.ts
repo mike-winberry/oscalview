1e1d789bfbfca2fc807926f2409c3fbb7cecb84a
mike-winberry <mwinberry@live.com>
2025-02-03 19:05:01 -0800

    fix(playwright): config webserver command && removed headless option

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-  use: {
-    headless: true,
-  },
+
   webServer: {
     command: 'pnpm run dev',
     port: 3000,
